825b8fe6c6a8604d8fa56a5df9ebc95410860db2
Kyle-T2804 <22190@my.sanctamaria.school.nz>
2025-08-07 12:44:07 +1200

    Add a temporary Scout page and update views to pass current user context for all mercs

--- NCEA Website Internal/web development/Flask_Web/website/views.py
+++ NCEA Website Internal/web development/Flask_Web/website/views.py
@@ -56,36 +56,36 @@ def delete_note():
 
 @views.route('/scout')
 def scout():
-    return render_template('scout.html')
+    return render_template('scout.html', user=current_user)
 
 @views.route('/soldier')
 def soldier():
-    return render_template('soldier.html')
+    return render_template('soldier.html', user=current_user)
 
 @views.route('/pyro')
 def pyro():
-    return render_template('pyro.html')
+    return render_template('pyro.html', user=current_user)
 
 @views.route('/demoman')
 def demoman():
-    return render_template('demoman.html')
+    return render_template('demoman.html', user=current_user)
 
 @views.route('/heavy')
 def heavy():
-    return render_template('heavy.html')
+    return render_template('heavy.html', user=current_user)
 
 @views.route('/engineer')
 def engineer():
-    return render_template('engineer.html')
+    return render_template('engineer.html', user=current_user)
 
 @views.route('/medic')
 def medic():
-    return render_template('medic.html')
+    return render_template('medic.html', user=current_user)
 
 @views.route('/sniper')
 def sniper():
-    return render_template('sniper.html')
+    return render_template('sniper.html', user=current_user)
 
 @views.route('/spy')
 def spy():
-    return render_template('spy.html')
+    return render_template('spy.html', user=current_user)
